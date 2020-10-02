2f47af1db570899f9119f3c33832aff50dc91a0e
aansh-01 <62788676+aansh-01@users.noreply.github.com>
2020-10-02 05:39:35 +0530

    Update routes.js

--- Routes/routes.js
+++ Routes/routes.js
@@ -4,26 +4,24 @@ const Log = require("../models/astroLogs")
 
 
 //Read All Click Logs from Server
-router.get("/", async (req, res) => {
+router.get("/", async(req, res) => {
     try {
         const readLogs = await Log.find();
         res.json(readLogs);
-    }
-    catch(err) {
-        res.status(500).json({ message : err.name });
+    } catch (err) {
+        res.json({ message: err.name });
     }
 });
 
 
 
 //Read Monthly Logs
-router.get("/month", async (req, res) => {
+router.get("/month", async(req, res) => {
     try {
-        const readLogs = await Log.find({ "month" : req.query.month });
+        const readLogs = await Log.find({ "month": req.query.month });
         res.json(readLogs);
-    }
-    catch(err) {
-        res.json({ message : err.name });
+    } catch (err) {
+        res.json({ message: err.name });
     }
 });
 
@@ -31,7 +29,7 @@ router.get("/month", async (req, res) => {
 
 
 //Send Click Logs to Server
-router.post('/', async (req, res) => {
+router.post('/', async(req, res) => {
     const log = new Log({
         action: req.body.action,
         id: req.body.id,
@@ -47,12 +45,32 @@ router.post('/', async (req, res) => {
 
     try {
         const saveLog = await log.save()
-        res.status(201).json(saveLog);
+        res.json(saveLog);
+    } catch (err) {
+        res.json({ message: err.name });
     }
-    catch(err) {
-        res.status(400).json({ message : err.name });
+});
+
+router.get('/delete/:id', async(req, res) => {
+    try {
+        const LID = req.params.id;
+        Log.findOneAndDelete({ id: LID })
+            .then(() => {
+                res.status(200).json({
+                    message: "Log deleted successfully"
+                });
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: err.name
+                });
+            })
+    } catch (err) {
+        res.status(500).json({
+            message: err.name
+        });
     }
-})
+});
 // Edit One Route PUT version
 router.put("/put/:id", async (req, res) => {
     try {
@@ -64,11 +82,35 @@ router.put("/put/:id", async (req, res) => {
 });
 // Edit One Route PATCH version
 router.patch("/patch/:id", async (req, res) => {
-    if (req.body.firstname != null) {
-        res.log.firstname = req.body.firstname;
+    if (req.body.action != null) {
+        res.log.action = req.body.firstname;
     }
-    if (req.body.lastname != null) {
-        res.log.lastname = req.body.lastname;
+    if (req.body.id != null) {
+        res.log.id = req.body.lastname;
+    }
+    if (req.body.class != null) {
+        res.log.class = req.body.lastname;
+    }
+    if (req.body.day != null) {
+        res.log.day = req.body.lastname;
+    }
+    if (req.body.date != null) {
+        res.log.date = req.body.lastname;
+    }
+    if (req.body.month != null) {
+        res.log.month = req.body.lastname;
+    }
+    if (req.body.year != null) {
+        res.log.year = req.body.lastname;
+    }
+    if (req.body.hour != null) {
+        res.log.hour = req.body.lastname;
+    }
+    if (req.body.minute != null) {
+        res.log.minute = req.body.lastname;
+    }
+    if (req.body.second != null) {
+        res.log.second = req.body.lastname;
     }
     try {
         const updatedLog = await res.log.save();
@@ -77,4 +119,6 @@ router.patch("/patch/:id", async (req, res) => {
         res.status(400).json({ message: err.message });
     }
 });
+
+
 module.exports = router;
